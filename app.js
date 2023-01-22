9af5df3dd8b3781d337d7c5028e5366bf4efdea4
pmajkowski <majkowski_20@o2.pl>
2023-01-22 14:26:53 +0100

    Poprawiono dodawanie i odejmowanie wyniku

--- app.js
+++ app.js
@@ -21,6 +21,7 @@
 // // losowa do 20 i całkowita
 // val=Math.ceil(Math.random()*20);
 let palette =['red','green','black','pink','orange','grey','blue','yellow'];
+let result = 0;
 const startRandom=(target)=>
 {
 let val1 = Math.floor(Math.random()*8)
@@ -30,9 +31,10 @@ document.querySelector('#nazwa-koloru').innerText=palette[val1];
 
 document.querySelector('#nazwa-koloru').style.color=palette[val2];
 
+document.querySelector('#start-text').innerText=' ';
+
 
 }
-    let result = 0;
 const startClick=(target)=>{
     setTimeout(()=>{
         document.querySelector('#start-text').innerText=`Koniec gry! Twój wynik to ${result}`;
@@ -42,31 +44,42 @@ const startClick=(target)=>{
         document.querySelector('#summary').innerText=' ';
         document.querySelector('#nazwa-koloru').innerText=' ';
         document.querySelector('#but4').style.display='inline-block';
-    },20000)
+    },30000)
     document.querySelector('#but2').style.display='inline-block';
     document.querySelector('#but3').style.display='inline-block';
         document.querySelector('#summary').innerText=0;
         document.querySelector('#start-text').innerText='Rozpoczynamy grę!';
     }
-const yesClick=(target)=>{
+
+
+const yesNoClick=(target)=>{
     if(
         document.querySelector('#nazwa-koloru').innerText
         ===   
         document.querySelector('#nazwa-koloru').style.color
-
+&& target.value === 'yes'
     )
     {
     result++
-    document.querySelector('#summary').innerText=result;
-    document.querySelector('#start-text').innerText=' ';} else {
-        document.querySelector('#start-text').innerText=`Koniec gry! Twój wynik to ${result}`;
-        document.querySelector('#but2').style.display='none';
-        document.querySelector('#but3').style.display='none';
-        document.querySelector('#summary').innerText=' ';
-        document.querySelector('#nazwa-koloru').innerText=' ';
-    }
+} else if (
+    document.querySelector('#nazwa-koloru').innerText
+    !==   
+    document.querySelector('#nazwa-koloru').style.color
+&& target.value === 'no'
+) {
+    result++
 }
-const noClick=(target)=>{   document.querySelector('#start-text').innerText=' '};
+
+else {
+    result--
+        // document.querySelector('#but2').style.display='none';
+        // document.querySelector('#but3').style.display='none';
+        // document.querySelector('#summary').innerText=' ';
+        // document.querySelector('#nazwa-koloru').innerText=' ';
+    }
+    document.querySelector('#summary').innerText=result;
+    document.querySelector('#start-text').innerText=' ';
+}
 const returnClick=(target)=>{
     document.querySelector('#but1').style.display='inline-block';
         document.querySelector('#but2').style.display='inline-block';
