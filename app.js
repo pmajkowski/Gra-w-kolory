29045c0d74c3f2b99bdae8c37e6f1cb85aa6418f
pmajkowski <majkowski_20@o2.pl>
2023-01-27 20:27:34 +0100

    Poprawka błędnie dadawanych punktów.

--- app.js
+++ app.js
@@ -21,8 +21,8 @@
 // // losowa do 20 i całkowita
 // val=Math.ceil(Math.random()*20);
 let palette =['red','green','black','pink','orange','grey','blue','yellow'];
-let result = 0;
-const startRandom=(target)=>
+
+const startRandom=()=>
 {
 let val1 = Math.floor(Math.random()*8)
 let val2 = Math.floor(Math.random()*8)
@@ -36,6 +36,7 @@ document.querySelector('#start-text').innerText=' ';
 
 }
 const startClick=(target)=>{
+    result=0;
     setTimeout(()=>{
         document.querySelector('#start-text').innerText=`Koniec gry! Twój wynik to ${result}`;
         document.querySelector('#but1').style.display='none';
@@ -44,11 +45,12 @@ const startClick=(target)=>{
         document.querySelector('#summary').innerText=' ';
         document.querySelector('#nazwa-koloru').innerText=' ';
         document.querySelector('#but4').style.display='inline-block';
-    },30000)
+    },20000)
     document.querySelector('#but2').style.display='inline-block';
     document.querySelector('#but3').style.display='inline-block';
-        document.querySelector('#summary').innerText=0;
+        document.querySelector('#summary').innerText=' ';
         document.querySelector('#start-text').innerText='Rozpoczynamy grę!';
+        startRandom();
     }
 
 
@@ -72,13 +74,11 @@ const yesNoClick=(target)=>{
 
 else {
     result--
-        // document.querySelector('#but2').style.display='none';
-        // document.querySelector('#but3').style.display='none';
-        // document.querySelector('#summary').innerText=' ';
-        // document.querySelector('#nazwa-koloru').innerText=' ';
+ 
     }
     document.querySelector('#summary').innerText=result;
     document.querySelector('#start-text').innerText=' ';
+    startRandom();
 }
 const returnClick=(target)=>{
     document.querySelector('#but1').style.display='inline-block';
@@ -88,4 +88,5 @@ const returnClick=(target)=>{
         document.querySelector('#start-text').innerText=' ';
         document.querySelector('#nazwa-koloru').innerText='Zagraj w grę, w której Twoim zadaniem będzie odgadnięcie jak największej liczby kolorów w ciągu 20 sekund. Be a champ!';
         document.querySelector('#nazwa-koloru').style.color='black';
+        document.querySelector('#summary').innerText=0;
 }
